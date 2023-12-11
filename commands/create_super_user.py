7341f5f70bd98a6de7d848cb473fed7d5c4415c0
qustoo <zalevskiy@sfedu.ru>
2023-12-12 01:14:56 +0300

    asyncclick create user without start app

--- commands/create_super_user.py
+++ commands/create_super_user.py
@@ -1,38 +1,37 @@
 import os
 import sys
+
 # Харкод импорт из другой папки
-PROJECT_ROOT = os.path.abspath(os.path.join(
-                  os.path.dirname(__file__), 
-                  os.pardir)
-)
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 sys.path.append(PROJECT_ROOT)
 from sqlalchemy import insert
-from models import User,RoleType
+from models import User, RoleType
 from database import async_session_factory
 import asyncclick as click
 
-@click.command()
-@click.option("-f","--first_name",type=str,required=True)
-@click.option("-l","--last_name",type=str,required=True)
-@click.option("-e","--email",type=str,required=True)
-@click.option("-p","--phone",type=str,required=True)
-@click.option("-i","--iban",type=str,required=True)
-@click.option("-pa","--password",type=str,required=True)
 
-async def create_user(first_name,last_name,email,phone,iban,password):
-    user_data = {"first_name":first_name,
-                 "last_name":last_name,
-                 "email":email,
-                 "phone":phone,
-                 "iban":iban,
-                 "password":password,
-                 "role": RoleType.admin
-                 }
+@click.command()
+@click.option("-f", "--first_name", type=str, required=True)
+@click.option("-l", "--last_name", type=str, required=True)
+@click.option("-e", "--email", type=str, required=True)
+@click.option("-p", "--phone", type=str, required=True)
+@click.option("-i", "--iban", type=str, required=True)
+@click.option("-pa", "--password", type=str, required=True)
+async def create_user(first_name, last_name, email, phone, iban, password):
+    user_data = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email,
+        "phone": phone,
+        "iban": iban,
+        "password": password,
+        "role": RoleType.admin,
+    }
     async with async_session_factory() as session:
-        stmt =  insert(User).values(user_data)
+        stmt = insert(User).values(user_data)
         await session.execute(stmt)
         await session.commit()
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     create_user(_anyio_backend="asyncio")
